26deab41503fadc252848a6a0f2aa3f41d391f0b
181102274 <sxsqli@163.com>
2019-08-08 11:51:28 +0800

    删除cmake-build-debug,添加到.gitignore

--- init/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chewgum/slam/slam_ws/src/VINS-Fusion-master/init
-# Build directory: /home/chewgum/slam/slam_ws/src/VINS-Fusion-master/init/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- init/cmake-build-debug/catkin_generated/ordered_paths.cmake
+++
@@ -1,1 +0,0 @@
-set(ORDERED_PATHS "/home/chewgum/slam/slam_ws/devel/lib;/opt/ros/kinetic/lib")
